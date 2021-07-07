76368cac99d1417f731cc7a2b8295b83850a4146
JennyChao2021 <86760573+JennyChao2021@users.noreply.github.com>
2021-07-07 12:00:44 +0800

    Delete soundweb directory

--- soundweb/about/about.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $('.showMenu').on('click', function (event) {
-    event.preventDefault();
-    $('.botton').toggleClass('menu-show');
-  });
-  $(".botton li a").on("click", function (event) {
-    $('.botton').toggleClass('menu-show');
-  });
-}); 
